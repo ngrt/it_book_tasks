0add0301c963b0cc190dc162a1c6229fcd459f5c
ngrt <101061860+ngrt@users.noreply.github.com>
2025-11-10 13:44:39 +0300

    Add files via upload

---
+++ ex1 9par.py
@@ -0,0 +1,6 @@
+a = input("Напишите текст\n")
+d = a.count('.')
+q = a.count('?')
+e = a.count('!')
+result = d+q+e
+print("Количество предложений:", result)
---
+++ ex2 9par.py
@@ -0,0 +1,4 @@
+a = input('Текст:\n')
+b = a.split()
+result = len(b)
+print("Количество слов:", result)
